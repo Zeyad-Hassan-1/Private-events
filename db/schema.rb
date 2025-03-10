38998f6f18afc4e7241b7efda62f16281f6cf312
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-03-11 01:42:05 +0200

    Add event management features and Devise integration

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_10_220801) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
