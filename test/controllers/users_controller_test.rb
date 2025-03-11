1cefd38e25904542921a76d8d6e622ca219b6413
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-03-11 20:41:02 +0200

    fix formatting in users_controller_test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  
+
   setup do
     @user = users(:user_one)
     sign_in @user
